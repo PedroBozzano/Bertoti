e53ce691d82b3053747e51925c45106287c154d0
Pedro Bozzano <127267429+PedroBozzano@users.noreply.github.com>
2023-11-02 21:42:51 -0300

    Add files via upload

---
+++ engenhariaSW/TesteUMLExerc6/UML_Loja/src/Produto.java
@@ -0,0 +1,22 @@
+public class Produto {
+    private Integer idProduto;
+    private String descricaoProduto;
+
+    public Produto (Integer idProduto, String descricaoProduto){
+        this.idProduto=idProduto;
+        this.descricaoProduto=descricaoProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+}
